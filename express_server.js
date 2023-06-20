edf8b9323799e22c51e8df882ec9298ce82af430
Aasemaaneh <aasemaaneh8@gmail.com>
2023-06-20 18:51:16 -0400

    Filling Out the urls_index.ejs Template

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs"); //This tells the Express app to use EJS as its templating engine.
+app.set("view engine", "ejs"); //This tells the Express app to use EJS as its templating engine.ad
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -24,13 +24,8 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
-
-app.get("/set", (req, res) => {
- const a = 1;
- res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
- res.send(`a = ${a}`);
+//Sending Data to urls_index.ejs
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
 });
